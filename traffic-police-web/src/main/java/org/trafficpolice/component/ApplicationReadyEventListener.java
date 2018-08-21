4aef3cdc08a7c331be8276c2d11e472ebda78c40
zhangxiaofei <zhangxiaofei@ihangmei.com>
2018-08-21 09:16:06 +0800

    deploy

---
+++ traffic-police-web/src/main/java/org/trafficpolice/component/ApplicationReadyEventListener.java
@@ -0,0 +1,23 @@
+package org.trafficpolice.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhangxiaofei
+ * 2018年8月19日下午5:42:37
+ */
+@Component
+public class ApplicationReadyEventListener implements ApplicationListener<ApplicationReadyEvent> {
+
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationReadyEventListener.class);
+	
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent arg0) {
+		logger.info("############【学习平台】【启动完成】############");
+	}
+
+}
